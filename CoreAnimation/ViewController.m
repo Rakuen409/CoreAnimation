e49645a00fe5264e008faacedfed80f55d7ca949
Star <star@stardemacbook-pro.local>
2018-08-14 16:55:26 +0800

    nothing

--- CoreAnimation/ViewController.m
+++ CoreAnimation/ViewController.m
@@ -8,8 +8,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong)UITableView *tableview;
 @end
 
 @implementation ViewController
@@ -17,9 +17,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self.view addSubview:self.tableview];
     // Do any additional setup after loading the view, typically from a nib.
 }
-
+#pragma mark - TableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 1;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuse"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"reuse"];
+    }
+    if (indexPath.row == 0) {
+        cell.textLabel.text = @"变换";
+        cell.textLabel.textColor = [UIColor redColor];
+    }
+    return cell;
+}
+- (UITableView *)tableview{
+    if (!_tableview) {
+        _tableview = [[UITableView alloc] initWithFrame:self.view.bounds style:UITableViewStylePlain];
+        _tableview.dataSource = self;
+        _tableview.delegate = self;
+    }
+    return _tableview;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
